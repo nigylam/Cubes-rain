cc122a7707bf2bfd4825e610a981c582a1235b87
nigylam <malygin.vadim2010@yandex.ru>
2025-06-18 13:19:46 +0300

    ColorChanger removed

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,15 +1,30 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody))]
 public class Cube : MonoBehaviour
 {
+    private readonly string IsCollidedName = "isCollided";
+
     public event Action<Cube, ContactPoint> Collided;
     public event Action Disabled;
+
+    private Animator _animator;
 
     private bool _isCollided = false;
 
     public bool IsCollided => _isCollided;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool(IsCollidedName, _isCollided);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
